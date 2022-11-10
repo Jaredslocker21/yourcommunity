37bd3936403836e9dd3a4c2861df41ac7003fd1f
Jaredslocker21 <jaredslocker21@gmail.com>
2022-11-10 13:56:38 +0000

    add class MemberList too views.py

--- blog/admin.py
+++ blog/admin.py
@@ -13,6 +13,19 @@ class MemberAdmin(SummernoteModelAdmin):
     prepopulated_fields = {'slug': ('title',)}
     list_filter = ('status', 'created_on')
     summernote_fields = ('blurb',)
-    
 
-# Register your models here.
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    """
+    This class displays admin model shown in the admin panel
+        for the admin
+    """
+    list_display = ('name', 'body', 'post', 'created_on', 'approved')
+    list_filter = ('approved', 'created_on')
+    search_fields = ('name', 'email', 'body')
+    actions = ['approve_comments']  # displays approve comments
+
+    def approve_comments(self, request, queryset):
+        """ Comments to be approved """
+        queryset.update(approved=True)
