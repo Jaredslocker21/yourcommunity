a65846e0096e4793f7dd1e8f84857e9a1dd954af
Jaredslocker21 <jaredslocker21@gmail.com>
2022-11-11 10:26:47 +0000

    add email verification is none in settings.py

--- yourcommunity/settings.py
+++ yourcommunity/settings.py
@@ -30,6 +30,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
+ACCOUNT_EMAIL_VERIFICATION = 'none'
 
 ALLOWED_HOSTS = ["yourcommunityblog.herokuapp.com", "localhost"]
 
