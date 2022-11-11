1b83b7a002a77e6caa057a2db298c343030d5114
Jaredslocker21 <jaredslocker21@gmail.com>
2022-11-11 13:21:11 +0000

    add post method too comments

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, get_object_or_404
 from django.views import generic, View
 from .models import Member
+from .forms import CommentForm
 # Create your views here.
 
 
@@ -24,13 +25,43 @@ class MemberDetail(View):
 
         return render(
             request,
-            'member_detail.html',
+            "member_detail.html",
             {
                 "member": member,
                 "comments": comments,
                 "commented": False,
                 "liked": liked,
-                #"comment_form": CommentForm(),
-                #"member_form": MemberForm(),
-            }
-        )     
+                "comment_form": CommentForm()
+            },
+        )
+
+    def post(self, request, slug, *args, **kwargs):
+        queryset = Member.objects.filter(status=1)
+        member = get_object_or_404(queryset, slug=slug)
+        comments = member.comments.filter(approved=True).order_by('created_on')
+        liked = False
+        if member.likes.filter(id=self.request.user.id).exists():
+            liked = True
+
+        comment_form = CommentForm(data=request.POST)
+
+        if comment_form.is_valid():
+            comment_form.instance.email = request.user.email
+            comment_form.instance.name = request.user.name
+            comment = comment_form.save(commit=False)
+            comment.member = member
+            comment = save()
+        else:
+            comment_form = CommentForm()  
+
+        return render(
+            request,
+            "member_detail.html",
+            {
+                "member": member,
+                "comments": comments,
+                "commented": True,
+                "liked": liked,
+                "comment_form": CommentForm()
+            },
+        )
