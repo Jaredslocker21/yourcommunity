a4aab1d2134a48c9ad4baa35f2f02b849e3a07d8
Jaredslocker21 <jaredslocker21@gmail.com>
2022-11-11 09:12:28 +0000

    add Member detail view in views.py

--- blog/views.py
+++ blog/views.py
@@ -1,11 +1,36 @@
-from django.shortcuts import render
-from django.views import generic
+from django.shortcuts import render, get_object_or_404
+from django.views import generic, View
 from .models import Member
 # Create your views here.
 
 
 class MemberList(generic.ListView):
+    """ View code for Member Home page and site pagination """
     model = Member
     queryset = Member.objects.filter(status=1).order_by('-created_on')
     template_name = 'index.html'
     paginate_by = 8
+
+
+class MemberDetail(View):
+    """ View code for Member Page """
+    def get(self, request, slug, *args, **kwargs):
+        queryset = Member.objects.filter(status=1)
+        member = get_object_or_404(queryset, slug=slug)
+        comments = member.comments.filter(approved=True).order_by('created_on')
+        liked = False
+        if member.likes(id=self.request.user.id).exists():
+            liked = True
+
+        return render(
+            request,
+            'member_detail.html',
+            {
+                "member": member,
+                "comments": comments,
+                "commented": False,
+                "liked": liked,
+                #"comment_form": CommentForm(),
+                #"member_form": MemberForm(),
+            }
+        )     
