2702e59093e4e80fe909abf68b7b462ee57c9dbc
Jaredslocker21 <jaredslocker21@gmail.com>
2022-11-12 13:57:46 +0000

    add create_member views.py, path urls.py

--- blog/urls.py
+++ blog/urls.py
@@ -4,6 +4,7 @@ from django.urls import path
 urlpatterns = [
     path("", views.MemberList.as_view(), name='home'),
     path('about/', views.about, name='about'),
+    path('create_member/', views.create_member, name='create_member'),
     path('<slug:slug>/', views.MemberDetail.as_view(), name='member_detail'),
     path('like/<slug:slug>', views.MemberLike.as_view(), name='member_like'),
 ]
