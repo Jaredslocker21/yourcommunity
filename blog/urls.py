d0fbf7d87525521d1fd4e26cba246ee087e21ca9
Jaredslocker21 <jaredslocker21@gmail.com>
2022-11-12 09:37:26 +0000

    replaced path in correct order urls.py

--- blog/urls.py
+++ blog/urls.py
@@ -3,7 +3,8 @@ from django.urls import path
 
 urlpatterns = [
     path("", views.MemberList.as_view(), name='home'),
+    path('about/', views.about, name='about'),
     path('<slug:slug>/', views.MemberDetail.as_view(), name='member_detail'),
     path('like/<slug:slug>', views.MemberLike.as_view(), name='member_like'),
-    path('about/', views.about, name='about'),
+    
 ]
