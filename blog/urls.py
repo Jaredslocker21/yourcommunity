1af46ef189eb8ba3ea4f51c45b539124237455a5
Jaredslocker21 <jaredslocker21@gmail.com>
2022-11-13 15:33:39 +0000

    add path edit member urls.py

--- blog/urls.py
+++ blog/urls.py
@@ -5,6 +5,7 @@ urlpatterns = [
     path("", views.MemberList.as_view(), name='home'),
     path('about/', views.about, name='about'),
     path('create_member/', views.create_member, name='create_member'),
+    path('edit_member/<slug:slug>', views.edit_member, name='edit_member'),
     path('<slug:slug>/', views.MemberDetail.as_view(), name='member_detail'),
     path('like/<slug:slug>', views.MemberLike.as_view(), name='member_like'),
 ]
