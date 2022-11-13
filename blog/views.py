6ae0f0db4784bf4b9bdcc9c881c4f91087eee625
Jaredslocker21 <jaredslocker21@gmail.com>
2022-11-13 16:41:08 +0000

    import redirect too views.py

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, get_object_or_404, reverse
+from django.shortcuts import render, redirect, get_object_or_404, reverse
 from django.views import generic, View
 from django.http import HttpResponseRedirect
 from .models import Member
