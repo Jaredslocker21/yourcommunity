a7cf2b7c150675293a32cfbac314fd45a211e447
Jaredslocker21 <jaredslocker21@gmail.com>
2022-11-26 15:48:13 +0000

    comments are added to apps.py

--- blog/apps.py
+++ blog/apps.py
@@ -1,6 +1,7 @@
 from django.apps import AppConfig
 
 
+# Model used from "I think therefore I blog" walkthrough.
 class BlogConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'blog'
